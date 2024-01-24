d028a3117b88f037608fb94c8028430df83c7f37
Sohom <sohomdatta1+git@gmail.com>
2024-01-25 00:18:09 +0530

    cautiously disable more workers

--- jobs.py
+++ jobs.py
@@ -9,7 +9,7 @@ from multiprocessing import get_context
 
 mp = get_context('spawn')
 
-exec_pool = ProcessPoolExecutor(max_workers=500, mp_context=mp)
+exec_pool = ProcessPoolExecutor(max_workers=100, mp_context=mp)
 
 
 def async_main(json_data: dict, num: int, rid: UUID) -> None:
