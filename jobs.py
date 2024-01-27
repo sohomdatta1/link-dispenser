5b06d8e9881071d870b9513ed46c28647232eb6e
Sohom <sohomdatta1+git@gmail.com>
2024-01-28 01:32:29 +0530

    reset caching to normal levels

--- jobs.py
+++ jobs.py
@@ -12,7 +12,7 @@ def crawl_page(json_data: dict, num: int, rid: UUID) -> None:
     url = json_data['url']
     json_data['url_info'] = analyze_url(url)
     r.sadd(REDIS_KEY_PREFIX + str(rid), str(num) + '|' + json.dumps(json_data))
-    r.expire(REDIS_KEY_PREFIX + str(rid), timedelta(days=365))
+    r.expire(REDIS_KEY_PREFIX + str(rid), timedelta(days=1))
 
 
 def push_analysis(article_name: str):
