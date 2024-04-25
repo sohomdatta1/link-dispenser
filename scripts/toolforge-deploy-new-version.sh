ca9305eb9e3bcea1608d4d62332bbd3d470580c3
Sohom <sohomdatta1+git@gmail.com>
2024-04-25 14:43:16 -0400

    fix deploy script

--- scripts/toolforge-deploy-new-version.sh
+++ scripts/toolforge-deploy-new-version.sh
@@ -3,6 +3,5 @@ set -ex
 toolforge build start https://gitlab.wikimedia.org/toolforge-repos/link-dispenser
 cd link-dispenser
 toolforge webservice restart
-cd link-dispenser
 toolforge jobs delete crawljob
 toolforge jobs load jobs.yaml
